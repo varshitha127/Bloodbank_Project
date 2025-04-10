f2c689d0e23991aa3e6ad5300e505bbb7cf92191
Varshitha <165037791+varshitha127@users.noreply.github.com>
2025-04-10 13:38:17 +0530

    urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,3 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('polls.urls')),
 ]
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
